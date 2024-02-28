73bb8274fbb207f72231b7fb413bad952875ddd6
sanil <52231034+Slatefeather320@users.noreply.github.com>
2024-02-28 20:50:03 +0530

    Update LavaLamp.py

--- LavaLamp.py
+++ LavaLamp.py
@@ -1,9 +1,20 @@
 import pygame, math
 width = 300; height = 600
 window = pygame.display.set_mode((width, height)) 
-pygame.display.set_caption('Desktop Lavalamp')
+pygame.display.set_caption('Boids')
+
+##########################################
+#USER SETTINGS
+bounce = True #Set to true if you want boids to bounce off of window edge
+allignInfluence = 0.01
+seperateInfluence = 0.001
+cohesionInfluence = 0.001
+allignRadius = 100
+seperateRadius = 15  
+cohesionRadius = 100
 lava = (255, 153, 112)
 lamp = (46, 2, 38)
+##########################################
 
 class Boids:
     def __init__(self, index, vel, pos):
@@ -16,32 +27,51 @@ instances = []
 def makeNewBoid(index, vel=pygame.Vector2(0,0), pos=pygame.Vector2(width/2,height/2)):
     instances.append(Boids(index, vel, pos))
 
-def allignSeperate(boid, allignInfluence=0.14,seperateInfluence=0.001,allignRadius=100,seperateRadius=40):
+def asc(boid, allignInfluence=0.1,seperateInfluence=0.005, cohesionInfluence=0.001, allignRadius=100,seperateRadius=20,  cohesionRadius=100):
     allignDir = pygame.Vector2()
     seperateDir = pygame.Vector2()
+    cohesianAverage = pygame.Vector2(0,0)
+    count = 0
     for otherBoid in instances:
         if otherBoid != boid:
-            if (otherBoid.pos - boid.pos).magnitude() < allignRadius:
+            dist = (otherBoid.pos - boid.pos).magnitude()
+            if dist < allignRadius:
                 allignDir = allignDir + otherBoid.vel
-            if (otherBoid.pos - boid.pos).magnitude() < seperateRadius:
+            if dist < seperateRadius:
                 seperateDir = seperateDir + (boid.pos - otherBoid.pos)
+            if dist < cohesionRadius:
+                cohesianAverage = cohesianAverage + otherBoid.pos
+                count+=1
+    if cohesianAverage.magnitude()>0:
+        cohesianAverage /= count
     if allignDir.magnitude() > 0:
-        boid.vel = ((allignDir.normalize()*allignInfluence)+ (seperateDir*seperateInfluence) + boid.vel.normalize()).normalize()*boid.vel.magnitude()
+        boid.vel = ((allignDir.normalize()*allignInfluence)+ (seperateDir*seperateInfluence) + ((cohesianAverage-boid.pos)*cohesionInfluence) + boid.vel.normalize()).normalize()*boid.vel.magnitude()
     
 
 def moveBoids():
-    hitbox = 35
     for boid in instances:
         boid.pos = boid.pos + boid.vel
-        if boid.pos.x > width-hitbox:
-            boid.vel.x *= -1
-        if boid.pos.x < hitbox:
-            boid.vel.x *= -1
-        if boid.pos.y > height-hitbox:
-            boid.vel.y *= -1
-        if boid.pos.y < hitbox:
-            boid.vel.y *= -1
-        allignSeperate(boid)
+        if boid.pos.x > width:
+            if bounce:
+                boid.vel.x = abs(boid.vel.x)*-1
+            else:
+                boid.pos.x = 0
+        if boid.pos.x < 0:
+            if bounce:
+                boid.vel.x = abs(boid.vel.x)
+            else:
+                boid.pos.x = width
+        if boid.pos.y > height:
+            if bounce:
+                boid.vel.y = abs(boid.vel.y)*-1
+            else:
+                boid.pos.y = 0
+        if boid.pos.y < 0:
+            if bounce:
+                boid.vel.y = abs(boid.vel.y)
+            else:
+                boid.pos.y = height
+        asc(boid,allignInfluence,seperateInfluence,cohesionInfluence,allignRadius,seperateRadius,cohesionRadius)
 
 def renderBoids():
     for Boid in instances:
@@ -50,10 +80,10 @@ def renderBoids():
 def splat(x=100):
     for i in range(0,x):
         theta = 2 * math.pi * i/x
-        mag = 2.5
+        mag = 3
         xcomp = mag*math.sin(theta)
         ycomp = mag*math.cos(theta)
-        makeNewBoid(i, pygame.Vector2(xcomp,ycomp))
+        makeNewBoid(i, pygame.Vector2(xcomp,ycomp),pygame.Vector2((width/2)+xcomp*25,(height/2)+ycomp*25))
 
 splat(150)
 
